4f7e9b40a3098d353d70c4e4d364e055264be5f6
Brian-Medina-is-sigma <Oscarc151@nycstudents.net>
2025-06-03 11:04:00 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
   console.log(req.body);
-  const { name, description } = req.body;
+  const { title, description } = req.body;
   const task = await taskModel.addTask(title, description);
   res.status(201).json(task);
 });
