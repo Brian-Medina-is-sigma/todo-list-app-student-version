a380fc208159f293f5f3b02927a69cf2a97d6098
Brian-Medina-is-sigma <Oscarc151@nycstudents.net>
2025-06-03 20:24:09 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,13 +3,13 @@ const router = express.Router();
 // There is a bug in line 4 you need to fix it
 const taskModel = require("../models/taskModel");
 
-//Write a comment describing the purpose of this route
+//Use the router to send a request to let it respond to the user back, and the computer will take its time to get the task done as the user expand the task requirements. The website is just a website, and not in the server just, yet. It will be connected by using the URL end points to connect to a brower, google chrome.
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
   res.json(tasks);
 });
 
-// Write a comment describing the purpose of this route
+// The server is in URL, which most websites have https on it; using this code from the server, the HTML will respond from it. The code makes URL end points for the user to navigate to a browser.
 const express = require('express');
 const app = express();
 
