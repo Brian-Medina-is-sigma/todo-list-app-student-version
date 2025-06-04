4d87142efa03fac68530eb96f10a1e0beb4f9c6c
Brian-Medina-is-sigma <Oscarc151@nycstudents.net>
2025-06-03 20:47:07 -0400

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,6 +1,6 @@
 const db = require("../db");
 
-//Add a comment describing what this function does and what does it return
+//The request from this server is select the tasks in order by created at DESC, and it will select all the task. The * means every tasks, which every task that people put in the website will let the computer put them in order. The respond is it must complete the request if it's clear for the computer to do it. The tasks will be returned into rows creating a huge lists that cause Murphy to stress out because there's too many tasks to handle.
 const getTasks = async () => {
   const res = await db.query(
     //correct this SQL query to select all tasks from the database
@@ -9,7 +9,7 @@ const getTasks = async () => {
   return res.rows;
 };
 
-//Write a comment describing what this function insersts a new task into the database
+//The task will be for the title and description to wait for the computer to put 0 rows as it's responding to the request, and there's specicfic tasks and values that will be ruturned from the computer (doing some coding binary stuff) too. It must fulfill this request. The request also need to be clear or else there will be a error.
 const addTask = async (title, description) => {
   const res = await db.query(
     "INSERT INTO tasks (title, description, is_complete, created_at) VALUES ($1, $2, false, NOW()) RETURNING *",
